4ddb52d3275f9f1d1de8a3e35bbdf1ee5ab5dcb7
Chris Eden <4263940+chreden@users.noreply.github.com>
2018-05-03 21:46:07 +0100

    Add an empty test so that the test project compiles

---
+++ trview.route.tests/RouteTests.cpp
@@ -0,0 +1,19 @@
+#include "CppUnitTest.h"
+
+#include <trview.route/Route.h>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace trview
+{
+    namespace route
+    {
+        namespace tests
+        {
+            TEST_CLASS(RouteTests)
+            {
+            public:
+            };
+        }
+    }
+}
