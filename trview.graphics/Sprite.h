49914fc471787e1edeb321b1c6b7cbaacbf9d4c9
chreden <4263940+chreden@users.noreply.github.com>
2018-05-16 13:09:51 +0100

    Add comments to sprites

--- trview.graphics/Sprite.h
+++ trview.graphics/Sprite.h
@@ -1,3 +1,8 @@
+/// @file Sprite.h
+/// @brief Used to render a texture to a render target.
+/// 
+/// Renders 2D textures to a render target. This can be to the screen or to a render target texture.
+
 #pragma once
 
 #include <wrl/client.h>
@@ -14,24 +19,54 @@ namespace trview
         struct IShaderStorage;
         class Texture;
 
+        /// Renders 2D textures to a render target.This can be to the screen or to a render target texture.
         class Sprite
         {
         public:
-            Sprite(const Microsoft::WRL::ComPtr<ID3D11Device>& device, const graphics::IShaderStorage& shader_storage, const uint32_t width, uint32_t height);
+            /// Create a new sprite.
+            /// @param device The D3D device to use to create resources.
+            /// @param shader_storage The interface to use to retrieve shaders.
+            /// @param host_width The width of the render target.
+            /// @param host_height The height of the render target.
+            Sprite(const Microsoft::WRL::ComPtr<ID3D11Device>& device, const graphics::IShaderStorage& shader_storage, uint32_t host_width, uint32_t host_height);
 
+            /// Render a texture to the current render target.
+            /// @param context The D3D context to use to render.
+            /// @param texture The texture to render.
+            /// @param x The x coordinate at which to render the texture.
+            /// @param y The y coordinate at which to render the texture.
+            /// @param width The width to which to scale the renderered texture.
+            /// @param height The height to which to scale the renderered texture.
+            /// @param colour Optional colour to apply to the texture when it is rendered.
             void render(const Microsoft::WRL::ComPtr<ID3D11DeviceContext>& context, const Texture& texture, float x, float y, float width, float height, DirectX::SimpleMath::Color colour = { 1,1,1,1 });
 
+            /// Get the width that the sprite considers the current render target to be.
+            /// @returns The current host width.
             uint32_t host_width() const;
 
+            /// Get the height that the sprite considers the current render target to be.
+            /// @returns The current host height.
             uint32_t host_height() const;
 
+            /// Set the host size - the size of the render target that is being rendered to.
+            /// @param width The width of the render target.
+            /// @param height The height of the render target.
             void set_host_size(uint32_t width, uint32_t height);
 
             Sprite(const Sprite&) = delete;
             Sprite& operator=(const Sprite&) = delete;
         private:
+            /// Create the buffer used to pass parameters to the shader.
+            /// @param device The D3D device to use to create the buffer.
             void create_matrix(const Microsoft::WRL::ComPtr<ID3D11Device>& device);
 
+            /// Update the buffer used to pass parameters to the shader.
+            /// @param context The D3D device context to use to update the buffer.
+            /// @param x The x position at which to render the texture.
+            /// @param y The y position at which to render the texture.
+            /// @param width The width to which to scale the renderered texture.
+            /// @param height The height to which to scale the renderered texture.
+            /// @param colour Optional colour to apply to the texture when it is rendered.
             void update_matrix(const Microsoft::WRL::ComPtr<ID3D11DeviceContext>& context, float x, float y, float width, float height, const DirectX::SimpleMath::Color& colour);
 
             Microsoft::WRL::ComPtr<ID3D11Buffer>       _vertex_buffer;
--- trview.graphics/SpriteSizeStore.h
+++ trview.graphics/SpriteSizeStore.h
@@ -1,3 +1,10 @@
+/// @file SpriteSizeStore.h
+/// @brief Stores the state of a sprite for the duration of an operation.
+/// 
+/// Stores the properties of a sprite for the duration of an operation. This way the host size
+/// for example can be stored before the operation begins and restored after the operation has
+/// finished. This is particularly useful when rendering to a different render target.
+
 #pragma once
 
 #include <cstdint>
@@ -8,20 +15,20 @@ namespace trview
     {
         class Sprite;
 
-        // Used to set and restore the host size of a sprite - useful when using the 
-        // sprite to render to a different render target.
+        /// Used to set and restore the host size of a sprite - useful when using the 
+        /// sprite to render to a different render target.
         class SpriteSizeStore final
         {
         public:
-            // Begin a sprite size store operation, setting the sprite to the new size specified
-            // after storing the old host size for later restoration.
-            // sprite: The sprite to operate on.
-            // new_width: The new host width of the sprite.
-            // new_height: The new host height of the sprite.
+            /// Begin a sprite size store operation, setting the sprite to the new size specified
+            /// after storing the old host size for later restoration.
+            /// sprite: The sprite to operate on.
+            /// new_width: The new host width of the sprite.
+            /// new_height: The new host height of the sprite.
             explicit SpriteSizeStore(Sprite& sprite, uint32_t new_width, uint32_t new_height);
 
-            // When this executes it will restore the host size of the sprite to what it was when
-            // the SpriteSizeStore was created.
+            /// When this executes it will restore the host size of the sprite to what it was when
+            /// the SpriteSizeStore was created.
             ~SpriteSizeStore();
         private:
             Sprite& _sprite;
